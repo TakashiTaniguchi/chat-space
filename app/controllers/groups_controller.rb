72a969a113616180ae945b756c29803be66f8cef
Takashi Taniguchi <Takashi@takashi-macbook-pro.local>
2018-04-28 17:53:00 +0900

    Add the update action to the groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,6 +20,15 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: "グループを編集しました"
+    else
+      render :edit
+    end
+  end
+
   private
   def group_params
     params.require(:group).permit(:name, { user_ids: [] })
