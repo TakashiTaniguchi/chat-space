6214b6118b7154303a7520b1d84b9c58a840d1b2
Takashi Taniguchi <Takashi@takashi-macbook-pro.local>
2018-05-06 14:33:09 +0900

    Add a before action “set_group” to the messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,13 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+
   def index
   end
   def create
+  end
+
+  private
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
