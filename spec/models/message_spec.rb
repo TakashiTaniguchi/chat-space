b929685f4f00b8c78dbd54bad161fa11d5666e7d
Takashi Taniguchi <Takashi@takashi-macbook-pro.local>
2018-05-16 11:42:17 +0900

    Add test codes which can save.

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
-describe Message do
+describe Message, type: :model do
   describe '#create' do
     context 'can save' do
       it "is valid without image" do
         expect(build(:message, image: nil)).to be_valid
+      end
+      it "is valid without body" do
+        expect(build(:message, body: nil)).to be_valid
+      end
+      it "is valid with body and image" do
+        expect(build(:message)).to be_valid
       end
     end
   end
