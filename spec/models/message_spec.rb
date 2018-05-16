717c9edadf5261989a3d721b17c162f0f6439679
Takashi Taniguchi <Takashi@takashi-macbook-pro.local>
2018-05-16 12:07:06 +0900

    Add test codes which can not save.

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -12,5 +12,22 @@ describe Message, type: :model do
         expect(build(:message)).to be_valid
       end
     end
+    context 'can not save' do
+      it "is invalid without body and image" do
+        message = build(:message, body: nil, image: nil)
+        message.valid?
+        expect(message.errors[:body_or_image]).to include("を入力してください")
+      end
+      it "is invalid without group_id" do
+        message = build(:message, group_id: nil)
+        message.valid?
+        expect(message.errors[:group]).to include("を入力してください")
+      end
+      it "is invalid without user_id" do
+        message = build(:message, user_id: nil)
+        message.valid?
+        expect(message.errors[:user]).to include("を入力してください")
+      end
+    end
   end
 end
