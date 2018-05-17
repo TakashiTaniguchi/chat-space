38e64ca6f8b22ad7a5c1533ffd86c2253319aaf8
Takashi Taniguchi <Takashi@takashi-macbook-pro.local>
2018-05-17 18:54:59 +0900

    Create messages_controller_spec.rb to test the messages controller.

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe MessagesController do
+  describe '#index' do
+    context 'log in' do
+      it 'assigns @group' do
+      end
+      it 'assigns @message' do
+      end
+      it 'renders index' do
+      end
+    end
+    context 'not log in' do
+      it 'redirects to new_user_session_path' do
+      end
+    end
+  end
+end
