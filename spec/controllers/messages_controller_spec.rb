4771db87bc9d418032f32430f6a2ebd5d67d0834
Takashi Taniguchi <Takashi@takashi-macbook-pro.local>
2018-05-17 19:17:12 +0900

    Add codes to log in user in the test environment.

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,15 +1,25 @@
 require 'rails_helper'
 
-describe MessagesController do
+describe MessagesController, type: :controller do
+  let(:user) { create(:user) }
+
   describe '#index' do
+
     context 'log in' do
+      before do
+        login_user user
+      end
+
       it 'assigns @group' do
       end
+
       it 'assigns @message' do
       end
+
       it 'renders index' do
       end
     end
+
     context 'not log in' do
       it 'redirects to new_user_session_path' do
       end
