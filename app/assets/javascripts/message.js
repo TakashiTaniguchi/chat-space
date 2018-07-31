368645f3cfac7ddbd8462b0725e3901ea4d199f9
Takashi Taniguchi <Takashi@takashi-macbook-pro.local>
2018-07-31 12:46:38 +0900

    Reduce the argument "createdAt" of the function "buildHTML". Integrate "var html" in the function "buildHTML" into one.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,40 +1,29 @@
 $(function(){
-  function buildHTML(message, createdAt){
-    if (message.image_url === null) {
-      var html = `
-<div class="chat-main__message" id="latest">
-  <div class="chat-main__message--top">
-    <div class="chat-main__message--name">
-      ${message.user_name}
-    </div>
-    <div class="chat-main__message--date-and-time">
-      ${createdAt}
-    </div>
-  </div>
-  <div class="chat-main__message--bottom">
-    ${message.body}
-  </div>
-</div>
-      `
-    } else {
-      var html = `
-<div class="chat-main__message" id="latest">
-  <div class="chat-main__message--top">
-    <div class="chat-main__message--name">
-      ${message.user_name}
-    </div>
-    <div class="chat-main__message--date-and-time">
-      ${createdAt}
-    </div>
-  </div>
-  <div class="chat-main__message--bottom">
-    ${message.body}
-    <br>
-    <img src="${message.image_url}">
-  </div>
-</div>
+  function buildHTML(message){
+    var createdAt = message.created_at.substr(0, 19).replace(/-/g, '/').replace('T', ' ');
+    var imgTag = ``
+    if (message.image_url !== null) {
+      imgTag = `
+      <br>
+      <img src="${message.image_url}">
       `
     }
+    var html = `
+    <div class="chat-main__message" id="latest">
+      <div class="chat-main__message--top">
+        <div class="chat-main__message--name">
+          ${message.user_name}
+        </div>
+        <div class="chat-main__message--date-and-time">
+          ${createdAt}
+        </div>
+      </div>
+      <div class="chat-main__message--bottom">
+        ${message.body}
+        ${imgTag}
+      </div>
+    </div>
+    `
     return html;
   }
 
@@ -51,8 +40,7 @@ $(function(){
       contentType: false
     })
     .done(function(data){
-      var createdAt = data.created_at.substr(0, 19).replace(/-/g, '/').replace('T', ' ');
-      var html = buildHTML(data, createdAt);
+      var html = buildHTML(data);
       $('.chat-main__body--messages').append(html)
       $('#message_body').val('')
       $('#message_image').val('')
